02c29ceb3614afb1c27e6f72a5d4bdc8fecc39ef
JakeYeatman <jake.yeatman@rds.global>
2018-12-20 15:13:21 +0000

    Add project files.

---
+++ ServerTesterApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerTesterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
